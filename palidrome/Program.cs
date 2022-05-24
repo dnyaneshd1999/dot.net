f759659382959cfa4d2f3b155c72034271eb08a9
dnyaneshd1999 <dnyaneshd1999@gmail.com>
2022-05-24 14:19:18 +0530

    first commit

--- palidrome/Program.cs
+++ palidrome/Program.cs
@@ -6,7 +6,21 @@ namespace palidrome
     {
         static void Main(string[] args)
         {
-            int r, sum = 0,temp;
+            string str = "";
+            Console.WriteLine("Enter the any string");
+            str = Console.ReadLine();
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            String str_reverse = new string(arr);
+            if(str.Equals(str_reverse))
+            {
+                Console.WriteLine("Given String is a palidrome");
+            }
+            else
+            {
+                Console.WriteLine("Given String is not a palidrome");
+            }
+            /* int r, sum = 0,temp;
             Console.Write("Enter the no=");
             int n = Convert.ToInt32(Console.ReadLine());
             temp = n;
@@ -25,7 +39,7 @@ namespace palidrome
             {
                 Console.WriteLine("No is not palidrone : "+sum);
             }
-            Console.WriteLine("Hello World!");
+           */
         }
     }
 }
