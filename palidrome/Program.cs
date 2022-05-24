d4b3c6163c51698032abe9dab22f9b53303bb39e
dnyaneshd1999 <dnyaneshd1999@gmail.com>
2022-05-24 12:22:33 +0530

    first commit

---
+++ palidrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace palidrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, sum = 0,temp;
+            Console.Write("Enter the no=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            temp = n;
+            while(n>0)
+            {
+                r = n % 10;
+                sum = (sum*10)+r;
+                n = n / 10;
+            }
+            if(temp==sum)
+            {
+                Console.WriteLine("NO is palidrone: "+sum);
+                
+            }
+            else
+            {
+                Console.WriteLine("No is not palidrone : "+sum);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
